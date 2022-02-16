921d839eaf95cb8a5d7225a14ece3507920d782e
pautrias <ptrias@fundaciobit.org>
2022-02-16 14:31:02 +0100

    Obrir versió #36

--- instanciagenerica-commons/src/main/java/org/fundaciobit/instanciagenerica/commons/utils/StaticVersion.java
+++ instanciagenerica-commons/src/main/java/org/fundaciobit/instanciagenerica/commons/utils/StaticVersion.java
@@ -4,7 +4,7 @@ package org.fundaciobit.instanciagenerica.commons.utils;
 */
 public interface StaticVersion {
 
-    public static final String VERSION="1.0.0";
+    public static final String VERSION="1.0.1";
 
     public static final String PROJECT_NAME="instanciagenerica";
 
