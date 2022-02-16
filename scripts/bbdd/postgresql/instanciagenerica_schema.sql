f1a7e92833315c1b37ab543e4d38177711a13788
pautrias <ptrias@fundaciobit.org>
2022-02-16 14:13:26 +0100

    Tancar versió #37

--- scripts/bbdd/postgresql/instanciagenerica_schema.sql
+++ scripts/bbdd/postgresql/instanciagenerica_schema.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.25
 -- Dumped by pg_dump version 9.3.25
--- Started on 2022-02-11 14:04:35
+-- Started on 2022-02-16 13:47:58
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,9 +14,87 @@ SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
+--
+-- TOC entry 1 (class 3079 OID 11750)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2004 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- TOC entry 171 (class 1259 OID 16395)
+-- Name: ige_fitxer_seq; Type: SEQUENCE; Schema: public; Owner: instanciagenerica
+--
+
+CREATE SEQUENCE public.ige_fitxer_seq
+    START WITH 1000
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ige_fitxer_seq OWNER TO instanciagenerica;
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
+
+--
+-- TOC entry 173 (class 1259 OID 16399)
+-- Name: ige_fitxer; Type: TABLE; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE TABLE public.ige_fitxer (
+    fitxerid bigint DEFAULT nextval('public.ige_fitxer_seq'::regclass) NOT NULL,
+    descripcio character varying(1000) DEFAULT NULL::character varying,
+    mime character varying(255) NOT NULL,
+    nom character varying(255) NOT NULL,
+    tamany bigint NOT NULL
+);
+
+
+ALTER TABLE public.ige_fitxer OWNER TO instanciagenerica;
+
+--
+-- TOC entry 174 (class 1259 OID 16407)
+-- Name: ige_idioma; Type: TABLE; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE TABLE public.ige_idioma (
+    idiomaid character varying(5) NOT NULL,
+    nom character varying(50) NOT NULL,
+    suportat boolean DEFAULT true NOT NULL,
+    ordre integer DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE public.ige_idioma OWNER TO instanciagenerica;
+
+--
+-- TOC entry 177 (class 1259 OID 16440)
+-- Name: ige_instanciagenerica_seq; Type: SEQUENCE; Schema: public; Owner: instanciagenerica
+--
+
+CREATE SEQUENCE public.ige_instanciagenerica_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ige_instanciagenerica_seq OWNER TO instanciagenerica;
 
 --
 -- TOC entry 178 (class 1259 OID 16442)
@@ -61,7 +139,7 @@ CREATE TABLE public.ige_instanciagenerica (
 ALTER TABLE public.ige_instanciagenerica OWNER TO instanciagenerica;
 
 --
--- TOC entry 1977 (class 0 OID 0)
+-- TOC entry 2005 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.instanciagenericaid; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -70,7 +148,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.instanciagenericaid IS 'Clau prim
 
 
 --
--- TOC entry 1978 (class 0 OID 0)
+-- TOC entry 2006 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.numregistre; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -79,7 +157,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.numregistre IS 'Numero de registr
 
 
 --
--- TOC entry 1979 (class 0 OID 0)
+-- TOC entry 2007 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.uuid; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -88,7 +166,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.uuid IS 'Identificador de la inst
 
 
 --
--- TOC entry 1980 (class 0 OID 0)
+-- TOC entry 2008 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.solicitantpersonafisica; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -98,7 +176,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.solicitantpersonafisica IS 'Per s
 
 
 --
--- TOC entry 1981 (class 0 OID 0)
+-- TOC entry 2009 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.solicitanttipusadminid; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -107,7 +185,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.solicitanttipusadminid IS 'Tipus 
 
 
 --
--- TOC entry 1982 (class 0 OID 0)
+-- TOC entry 2010 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.solicitantadminid; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -116,7 +194,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.solicitantadminid IS 'Nombre del 
 
 
 --
--- TOC entry 1983 (class 0 OID 0)
+-- TOC entry 2011 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.idiomaid; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -125,7 +203,7 @@ COMMENT ON COLUMN public.ige_instanciagenerica.idiomaid IS 'Idioma en que del qu
 
 
 --
--- TOC entry 1984 (class 0 OID 0)
+-- TOC entry 2012 (class 0 OID 0)
 -- Dependencies: 178
 -- Name: COLUMN ige_instanciagenerica.solicitantcif; Type: COMMENT; Schema: public; Owner: instanciagenerica
 --
@@ -134,7 +212,66 @@ COMMENT ON COLUMN public.ige_instanciagenerica.solicitantcif IS 'Numero CIF de l
 
 
 --
--- TOC entry 1843 (class 2606 OID 16450)
+-- TOC entry 172 (class 1259 OID 16397)
+-- Name: ige_traduccio_seq; Type: SEQUENCE; Schema: public; Owner: instanciagenerica
+--
+
+CREATE SEQUENCE public.ige_traduccio_seq
+    START WITH 1000
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ige_traduccio_seq OWNER TO instanciagenerica;
+
+--
+-- TOC entry 175 (class 1259 OID 16412)
+-- Name: ige_traduccio; Type: TABLE; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE TABLE public.ige_traduccio (
+    traduccioid bigint DEFAULT nextval('public.ige_traduccio_seq'::regclass) NOT NULL
+);
+
+
+ALTER TABLE public.ige_traduccio OWNER TO instanciagenerica;
+
+--
+-- TOC entry 176 (class 1259 OID 16416)
+-- Name: ige_traducciomap; Type: TABLE; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE TABLE public.ige_traducciomap (
+    traducciomapid bigint NOT NULL,
+    idiomaid character varying(10) NOT NULL,
+    valor character varying(4000)
+);
+
+
+ALTER TABLE public.ige_traducciomap OWNER TO instanciagenerica;
+
+--
+-- TOC entry 1853 (class 2606 OID 16423)
+-- Name: ige_fitxer_pk; Type: CONSTRAINT; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+ALTER TABLE ONLY public.ige_fitxer
+    ADD CONSTRAINT ige_fitxer_pk PRIMARY KEY (fitxerid);
+
+
+--
+-- TOC entry 1856 (class 2606 OID 16425)
+-- Name: ige_idioma_pk; Type: CONSTRAINT; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+ALTER TABLE ONLY public.ige_idioma
+    ADD CONSTRAINT ige_idioma_pk PRIMARY KEY (idiomaid);
+
+
+--
+-- TOC entry 1866 (class 2606 OID 16450)
 -- Name: ige_instanciagenerica_pk; Type: CONSTRAINT; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -143,7 +280,41 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1844 (class 1259 OID 16506)
+-- TOC entry 1859 (class 2606 OID 16427)
+-- Name: ige_traduccio_pk; Type: CONSTRAINT; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+ALTER TABLE ONLY public.ige_traduccio
+    ADD CONSTRAINT ige_traduccio_pk PRIMARY KEY (traduccioid);
+
+
+--
+-- TOC entry 1864 (class 2606 OID 16429)
+-- Name: ige_traducmap_pk; Type: CONSTRAINT; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+ALTER TABLE ONLY public.ige_traducciomap
+    ADD CONSTRAINT ige_traducmap_pk PRIMARY KEY (traducciomapid, idiomaid);
+
+
+--
+-- TOC entry 1854 (class 1259 OID 16430)
+-- Name: ige_fitxer_pk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE INDEX ige_fitxer_pk_i ON public.ige_fitxer USING btree (fitxerid);
+
+
+--
+-- TOC entry 1857 (class 1259 OID 16431)
+-- Name: ige_idioma_pk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE INDEX ige_idioma_pk_i ON public.ige_idioma USING btree (idiomaid);
+
+
+--
+-- TOC entry 1867 (class 1259 OID 16506)
 -- Name: ige_instanciagenerica_pk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -151,7 +322,7 @@ CREATE INDEX ige_instanciagenerica_pk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1845 (class 1259 OID 16509)
+-- TOC entry 1868 (class 1259 OID 16509)
 -- Name: ige_instgen_fitxer1id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -159,7 +330,7 @@ CREATE INDEX ige_instgen_fitxer1id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1846 (class 1259 OID 16510)
+-- TOC entry 1869 (class 1259 OID 16510)
 -- Name: ige_instgen_fitxer2id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -167,7 +338,7 @@ CREATE INDEX ige_instgen_fitxer2id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1847 (class 1259 OID 16511)
+-- TOC entry 1870 (class 1259 OID 16511)
 -- Name: ige_instgen_fitxer3id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -175,7 +346,7 @@ CREATE INDEX ige_instgen_fitxer3id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1848 (class 1259 OID 16512)
+-- TOC entry 1871 (class 1259 OID 16512)
 -- Name: ige_instgen_fitxer4id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -183,7 +354,7 @@ CREATE INDEX ige_instgen_fitxer4id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1849 (class 1259 OID 16513)
+-- TOC entry 1872 (class 1259 OID 16513)
 -- Name: ige_instgen_fitxer5id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -191,7 +362,7 @@ CREATE INDEX ige_instgen_fitxer5id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1850 (class 1259 OID 16514)
+-- TOC entry 1873 (class 1259 OID 16514)
 -- Name: ige_instgen_fitxer6id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -199,7 +370,7 @@ CREATE INDEX ige_instgen_fitxer6id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1851 (class 1259 OID 16515)
+-- TOC entry 1874 (class 1259 OID 16515)
 -- Name: ige_instgen_fitxer7id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -207,7 +378,7 @@ CREATE INDEX ige_instgen_fitxer7id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1852 (class 1259 OID 16516)
+-- TOC entry 1875 (class 1259 OID 16516)
 -- Name: ige_instgen_fitxer8id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -215,7 +386,7 @@ CREATE INDEX ige_instgen_fitxer8id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1853 (class 1259 OID 16517)
+-- TOC entry 1876 (class 1259 OID 16517)
 -- Name: ige_instgen_fitxer9id_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -223,7 +394,7 @@ CREATE INDEX ige_instgen_fitxer9id_fk_i ON public.ige_instanciagenerica USING bt
 
 
 --
--- TOC entry 1854 (class 1259 OID 16508)
+-- TOC entry 1877 (class 1259 OID 16508)
 -- Name: ige_instgen_idiomaid_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
 --
 
@@ -231,7 +402,31 @@ CREATE INDEX ige_instgen_idiomaid_fk_i ON public.ige_instanciagenerica USING btr
 
 
 --
--- TOC entry 1855 (class 2606 OID 16451)
+-- TOC entry 1860 (class 1259 OID 16432)
+-- Name: ige_traduccio_pk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE INDEX ige_traduccio_pk_i ON public.ige_traduccio USING btree (traduccioid);
+
+
+--
+-- TOC entry 1861 (class 1259 OID 16433)
+-- Name: ige_traducciomap_idiomaid_fk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE INDEX ige_traducciomap_idiomaid_fk_i ON public.ige_traducciomap USING btree (idiomaid);
+
+
+--
+-- TOC entry 1862 (class 1259 OID 16434)
+-- Name: ige_traducciomap_pk_i; Type: INDEX; Schema: public; Owner: instanciagenerica; Tablespace: 
+--
+
+CREATE INDEX ige_traducciomap_pk_i ON public.ige_traducciomap USING btree (traducciomapid);
+
+
+--
+-- TOC entry 1879 (class 2606 OID 16451)
 -- Name: ige_instgen_fitxer_fitxer1_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -240,7 +435,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1857 (class 2606 OID 16466)
+-- TOC entry 1881 (class 2606 OID 16466)
 -- Name: ige_instgen_fitxer_fitxer2_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -249,7 +444,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1858 (class 2606 OID 16471)
+-- TOC entry 1882 (class 2606 OID 16471)
 -- Name: ige_instgen_fitxer_fitxer3_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -258,7 +453,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1859 (class 2606 OID 16476)
+-- TOC entry 1883 (class 2606 OID 16476)
 -- Name: ige_instgen_fitxer_fitxer4_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -267,7 +462,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1860 (class 2606 OID 16481)
+-- TOC entry 1884 (class 2606 OID 16481)
 -- Name: ige_instgen_fitxer_fitxer5_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -276,7 +471,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1861 (class 2606 OID 16486)
+-- TOC entry 1885 (class 2606 OID 16486)
 -- Name: ige_instgen_fitxer_fitxer6_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -285,7 +480,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1862 (class 2606 OID 16491)
+-- TOC entry 1886 (class 2606 OID 16491)
 -- Name: ige_instgen_fitxer_fitxer7_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -294,7 +489,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1863 (class 2606 OID 16496)
+-- TOC entry 1887 (class 2606 OID 16496)
 -- Name: ige_instgen_fitxer_fitxer8_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -303,7 +498,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1864 (class 2606 OID 16501)
+-- TOC entry 1888 (class 2606 OID 16501)
 -- Name: ige_instgen_fitxer_fitxer9_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -312,7 +507,7 @@ ALTER TABLE ONLY public.ige_instanciagenerica
 
 
 --
--- TOC entry 1856 (class 2606 OID 16456)
+-- TOC entry 1880 (class 2606 OID 16456)
 -- Name: ige_instgen_idioma_idioma_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
 --
 
@@ -320,7 +515,28 @@ ALTER TABLE ONLY public.ige_instanciagenerica
     ADD CONSTRAINT ige_instgen_idioma_idioma_fk FOREIGN KEY (idiomaid) REFERENCES public.ige_idioma(idiomaid);
 
 
--- Completed on 2022-02-11 14:04:36
+--
+-- TOC entry 1878 (class 2606 OID 16435)
+-- Name: ige_traducmap_traduccio_fk; Type: FK CONSTRAINT; Schema: public; Owner: instanciagenerica
+--
+
+ALTER TABLE ONLY public.ige_traducciomap
+    ADD CONSTRAINT ige_traducmap_traduccio_fk FOREIGN KEY (traducciomapid) REFERENCES public.ige_traduccio(traduccioid);
+
+
+--
+-- TOC entry 2003 (class 0 OID 0)
+-- Dependencies: 6
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2022-02-16 13:47:58
 
 --
 -- PostgreSQL database dump complete
--- scripts/bbdd/postgresql/instanciagenerica_update_0.0.0_1.0.0.sql
+++
@@ -1,5 +0,0 @@
-
-
--- 2022-02-14   Nom de la issues #666
-
-ALTER TABLE ige_instanciagenerica ADD COLUMN solicitantcif character varying(50);
